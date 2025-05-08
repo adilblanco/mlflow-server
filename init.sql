6919fbe179e0b1748688b4d2d192b7ffe5e8b17b
Adil Labiad <adillabiad@gmail.com>
2025-05-07 22:04:43 -0400

    refactor

--- init.sql
+++
@@ -1,8 +0,0 @@
--- init.sql
-CREATE USER mlflow WITH PASSWORD 'mlflow';
-CREATE DATABASE mlflowdb;
-GRANT ALL PRIVILEGES ON DATABASE mlflowdb TO mlflow;
-
--- Optionnel : Tables spécifiques si nécessaire
--- \connect mlflowdb
--- CREATE TABLE IF NOT EXISTS experiments (...);
